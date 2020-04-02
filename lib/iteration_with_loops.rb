c2512aaa79152122bb7c3f4fea2f69d8dd4f99bb
Zakari Badiru <zbadiru@live.co.uk>
2020-04-02 19:52:45 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def find_even_values(src)
   row_index = 0 
   while row_index < src.count do 
     element_index = 0 
-    while element_index < src[row_index].count do 
+    while element_index < src[row_inde].count do 
       if src[row_index][element_index].even?
         p src[row_index][element_index]
       end
